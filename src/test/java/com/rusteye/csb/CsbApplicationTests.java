6b6cffb768b692b7d9abebde435d28c06387f8a4
Shenwy <swydesky@hotmail.com>
2021-03-05 16:10:52 +0800

    first commit

---
+++ src/test/java/com/rusteye/csb/CsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusteye.csb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
